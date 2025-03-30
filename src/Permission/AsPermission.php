98609bc8907c11ddc63faf8c96191e981350881f
tourze-ci <you@example.com>
2025-03-30 06:39:23 +0000

    feat(权限): 添加环境变量覆盖权限标题功能

--- src/Permission/AsPermission.php
+++ src/Permission/AsPermission.php
@@ -11,8 +11,12 @@ class AsPermission
     public function __construct(
         public string $name = '', // 名称
         public string $title = '', // 标题
+        public string $titleOverrideEnv = '',
         public array $children = [], // 自定义下级权限
         public array $includes = [], // 包含权限集，例如 A hasMany B，这个时候我们可以在这里声明B，那样子在权限那里就会在 A 的同层包含 B
     ) {
+        if (!empty($this->titleOverrideEnv) && isset($_ENV[$this->titleOverrideEnv])) {
+            $this->title = strval($_ENV[$this->titleOverrideEnv]);
+        }
     }
 }
